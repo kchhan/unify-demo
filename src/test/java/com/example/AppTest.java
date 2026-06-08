73abd99a2c9a938a071e4b562c728f863e5b0f82
Kevin Chhan <61722713+kchhan@users.noreply.github.com>
2026-06-08 14:13:42 -0700

    Create AppTest.java

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    @Test
+    public void testGetGreeting() {
+        App app = new App();
+        assertEquals("Hello World", app.getGreeting(), "The greeting message should match exactly");
+    }
+}
