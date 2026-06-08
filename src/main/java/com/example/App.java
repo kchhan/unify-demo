2be722a0afc0d5c3d03f0f640b6a59e169359a8d
Kevin Chhan <61722713+kchhan@users.noreply.github.com>
2026-06-08 14:12:59 -0700

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from CloudBees Unify!");
+    }
+
+    public String getGreeting() {
+        return "Hello World";
+    }
+}
